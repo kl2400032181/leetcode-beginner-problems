19ba4ea57433363075210a9a0eccfd2a2cb46ae9
CHALLAGUNDA NIKHITHA <2400032181@kluniversity.in>
2025-10-15 12:32:56 +0530

    Time: 15 ms (18.19%), Space: 44.6 MB (43.36%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean isPalindrome(String s) {
          s=s.toLowerCase().replaceAll("[^a-z0-9]","");
-        int l=0;
+        int p=0;
         int r=s.length()-1;
-        while(l<r){
-            if(s.charAt(l)!=s.charAt(r)){
+        while(p<r){
+            if(s.charAt(p)!=s.charAt(r)){
                 return false;
             }
-            l++;
+            p++;
             r--;
         }
         return true;
