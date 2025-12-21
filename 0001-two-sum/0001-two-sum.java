91984b72873849882f61718c155d9d36a32fa714
CHALLAGUNDA NIKHITHA <2400032181@kluniversity.in>
2025-12-22 00:50:15 +0530

    Rename variable 't' to 'target' in twoSum method

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,9 +1,9 @@
 class Solution {
-    public int[] twoSum(int[] nums, int t) {
+    public int[] twoSum(int[] nums, int target) {
        HashMap<Integer,Integer> hm=new HashMap<>();
        for(int i=0;i<nums.length;i++){
-        if(hm.containsKey(t-nums[i])){
-           return new int[]{hm.get(t-nums[i]),i};
+        if(hm.containsKey(target-nums[i])){
+           return new int[]{hm.get(target-nums[i]),i};
         }
         hm.put(nums[i],i);
        }
